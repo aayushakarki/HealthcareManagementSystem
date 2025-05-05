246291f31dd4a7d9bda09cc21c12e6736a6307a1
aayusha <aayushakarki24@gmail.com>
2025-05-05 22:04:10 +0545

    [i think i am done for the mids]

--- backend/router/vitalsRouter.js
+++ backend/router/vitalsRouter.js
@@ -3,7 +3,6 @@ import {
   getVitalsHistory,
   addVitals,
   getVitalsRecord,
-  updateVitalsRecord,
   deleteVitalsRecord,
 } from "../controller/vitalsController.js"
 import { isDoctorAuthenticated, isPatientAuthenticated } from "../middlewares/auth.js"
@@ -12,12 +11,10 @@ const router = express.Router()
 
 // Patient routes
 router.get("/history", isPatientAuthenticated, getVitalsHistory)
-router.post("/add", isPatientAuthenticated, addVitals)
 router.get("/:id", isPatientAuthenticated, getVitalsRecord)
 
 // Doctor routes
-router.post("/doctor/add", isDoctorAuthenticated, addVitals)
-router.put("/update/:id", isDoctorAuthenticated, updateVitalsRecord)
+router.post("/add", isDoctorAuthenticated, addVitals)
 router.delete("/delete/:id", isDoctorAuthenticated, deleteVitalsRecord)
 
 export default router
